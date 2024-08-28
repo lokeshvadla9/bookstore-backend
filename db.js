c25483963e1243d89a2a84fc621b70524c99a7d6
Lokesh V <lokeshv@Lokeshs-Air.dhost.uta.edu>
2024-08-28 17:00:10 -0500

    changed db credentials

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '51.81.160.157',
-  user: 'sxv3887_bookstore_user',
-  password: 'bookstore@123',
-  database: 'sxv3887_bookstore'
+  host: 'HOSTNAME',
+  user: 'USERNAME',
+  password: 'PASSWORD',
+  database: 'DATABASE'
 });
 
 module.exports = pool;

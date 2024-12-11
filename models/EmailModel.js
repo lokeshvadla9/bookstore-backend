d1e5e19349aecac47b8db869807eaedf1bb7d8a2
Lokesh Vadla <94765114+lokeshvadla9@users.noreply.github.com>
2024-12-11 10:42:41 -0600

    Update EmailModel.js

--- models/EmailModel.js
+++ models/EmailModel.js
@@ -1,13 +1,13 @@
 const sgMail = require('@sendgrid/mail');
 
 // Set your SendGrid API key
-sgMail.setApiKey('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');
+sgMail.setApiKey('API_KEY');
 
 
 const sendEmail= async (recipient, subject,html) => {
   const msg = {
-    to:recipient, // Replace with the recipient's email address
-    from: 'bookstoreuta@gmail.com', // Replace with your verified sender email address
+    to:recipient, 
+    from: 'YOUR_EMAIL', 
     subject: subject,
     html:html
   };
